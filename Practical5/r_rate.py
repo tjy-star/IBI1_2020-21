2297cac281f56ed785b4a85b51dd93a6a6678b37
tjy-star <3200110902bit@10.105.100.153>
2021-03-10 09:19:25 +0800

    r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,7 +1,11 @@
 r=1.1
+#r is the rate that people infected infect others
 x=84
+#x is the number of the initial students
 i=1
 while i <=5:
+#the loop will run five times
     x=x*r
     i=i+1
 print(x)
+#print the last number of people who are infected
