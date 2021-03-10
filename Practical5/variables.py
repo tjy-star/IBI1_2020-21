5510ce31cc767e3b3b44529df08bbfde44f7fa3d
tjy-star <3200110902bit@10.105.100.153>
2021-03-10 09:41:31 +0800

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,7 +3,13 @@ b=190784
 c=100321
 d=a-c
 e=b-a
+x=1<2
+y=1>2
 if d<e:
 	print("e is greater than d")
 else:
 	print("d is greater than e")
+z=(x and not y)or(y and not x)
+w=x!=y
+print(z)
+print(w)
